dafb7c5d9a832243aeac666fa7fd3f8750ec5c0a
kumaotto <won.artoiler09@icloud.com>
2022-05-07 20:42:42 +0900

    refactor nginx Dockerfile

--- src/views/AddArtworks.vue
+++ src/views/AddArtworks.vue
@@ -3,12 +3,12 @@
     <Header />
     <template v-if="saved">
       <section>
-        <h1>
-          作品が登録されました！
-        </h1>
+        <h1>作品が登録されました！</h1>
         <div :class="$style.buttons">
           <Button @click="onClickMypage">マイページへ戻る</Button>
-          <Button @click="onClickAddWorks" :isButtonRed="true">別の作品を登録する</Button>
+          <Button @click="onClickAddWorks" :isButtonRed="true"
+            >別の作品を登録する</Button
+          >
         </div>
       </section>
     </template>
@@ -23,14 +23,8 @@
           :filesLengthLimit="1"
           @uploadFile="upload"
         />
-        <TextFiled
-          v-model="artworkName"
-          :placeholder="'作品名'"
-        />
-        <TextFiled
-          v-model="author"
-          :placeholder="'作者名'"
-        />
+        <TextFiled v-model="artworkName" :placeholder="'作品名'" />
+        <TextFiled v-model="author" :placeholder="'作者名'" />
         <Button @click="onClickSubmit" :isButtonRed="true">追加</Button>
       </section>
     </template>
@@ -38,77 +32,79 @@
 </template>
 
 <script>
-// import { uploadWorks, downloadWorks } from '@/firebase/storage.js'
-import Header from '@/components/Organisms/Header'
-import FileUpload from '@/components/Molecules/FileUpload'
-import TextFiled from '@/components/Molecules/TextField'
-import Button from '@/components/Atoms/Button'
+import firebase from "firebase/app";
+import { uploadWorks, downloadWorks } from "@/firebase/storage.js";
+import Header from "@/components/Organisms/Header";
+import FileUpload from "@/components/Molecules/FileUpload";
+import TextFiled from "@/components/Molecules/TextField";
+import Button from "@/components/Atoms/Button";
 
 export default {
-  name: 'AddArtworks',
+  name: "AddArtworks",
   components: {
     FileUpload,
     TextFiled,
     Header,
-    Button
+    Button,
   },
   data() {
     return {
-      artwork: '',
-      artworkName: '',
-      author: '',
+      artwork: "",
+      artworkName: "",
+      author: "",
       saved: false,
-    }
+    };
   },
   methods: {
     upload(value) {
       this.artwork = value;
     },
     onClickMypage() {
-      this.$router.push('/mypage', () => {});
+      this.$router.push("/mypage", () => {});
     },
     onClickAddWorks() {
       this.$router.go({ path: this.$router.currentRoute.path, force: true });
     },
     onClickSubmit() {
-      // uploadWorks(this.artwork, this.artwork.name).on(
-      //   "state_changed",
-      //   null,
-      //   error => {
-      //     console.error(error);
-      //   },
-      //   () => {
-      //     downloadWorks(this.artwork.name)
-      //       .then((imageURL) => {
-      //         this.artwork = imageURL;
+      uploadWorks(this.artwork, this.artwork.name).on(
+        "state_changed",
+        null,
+        (error) => {
+          console.error(error);
+        },
+        () => {
+          downloadWorks(this.artwork.name).then((imageURL) => {
+            this.artwork = imageURL;
 
-      //         const colref = firebase.firestore().collection("exhibitions");
-      //         const userId = this.$route.params.id;
-      //         const data = {
-      //           artworks: firebase.firestore.FieldValue.arrayUnion({
-      //             artwork: this.artwork,
-      //             artworkName: this.artworkName,
-      //             author: this.author
-      //           })
-      //         };
-      //         colref.doc(userId).set(data, { merge: true })
-      //           .then(() => {
-      //             this.saved = true;
-      //           })
-      //           .catch((error) => {
-      //             console.error("Error adding document: ", error);
-      //           })
-      //       })
-      //   }
-      )
+            const colref = firebase.firestore().collection("exhibitions");
+            const userId = this.$route.params.id;
+            const data = {
+              artworks: firebase.firestore.FieldValue.arrayUnion({
+                artwork: this.artwork,
+                artworkName: this.artworkName,
+                author: this.author,
+              }),
+            };
+            colref
+              .doc(userId)
+              .set(data, { merge: true })
+              .then(() => {
+                this.saved = true;
+              })
+              .catch((error) => {
+                console.error("Error adding document: ", error);
+              });
+          });
+        }
+      );
     },
   },
-}
+};
 </script>
 
 <style module>
 .form {
-  background-color: #F4F4F4;
+  background-color: #f4f4f4;
   padding: 20px 10px;
 }
 .buttons {
